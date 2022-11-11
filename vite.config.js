340723d0bc8f5989698ab2a614bbc675ad6b064b
rjmchi <robert@rjmprogramming.com>
2022-11-11 02:43:59 -0600

    undoes last commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-import fs from 'fs';
-const host = 'myembroidery-production.up.railway.app/'
-
-
 export default defineConfig({
-    server: {
-        host,
-        hmr: { host },
-        https: {
-            key: fs.readFileSync(`/path/to/${host}.key`),
-            cert: fs.readFileSync(`/path/to/${host}.crt`),
-        },
-    },
     plugins: [
         laravel({
             input: [
