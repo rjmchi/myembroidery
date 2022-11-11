68dea084973d8be54972f2e348ff4d321593bb6b
rjmchi <robert@rjmprogramming.com>
2022-11-11 02:40:21 -0600

    updates vite config for assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+import fs from 'fs';
+const host = 'myembroidery-production.up.railway.app/'
+
+
 export default defineConfig({
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: fs.readFileSync(`/path/to/${host}.key`),
+            cert: fs.readFileSync(`/path/to/${host}.crt`),
+        },
+    },
     plugins: [
         laravel({
             input: [
